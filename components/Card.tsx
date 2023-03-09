1beace50971e07f4255ccfe89c73dc09e0906120
Tobias <tobias.c.ziegler@gmail.com>
2023-03-09 14:58:19 +0100

    Remove framer motion entirely

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,30 +1,14 @@
 import { PropsWithChildren } from 'react';
-import { motion, useReducedMotion } from 'framer-motion';
-import {
-  slideInVariants,
-  slideInVariantsReducedMotion,
-} from '@/shared/animations';
 
-const Card = ({ dark, children }: { dark?: boolean } & PropsWithChildren) => {
-  const prefersReducedMotion = useReducedMotion();
-
-  return (
-    <motion.div
-      variants={
-        prefersReducedMotion ? slideInVariantsReducedMotion : slideInVariants
-      }
-      initial="offscreen"
-      whileInView="onscreen"
-      viewport={{ once: true }}
-      className={`mb-8 w-full max-w-3xl items-center rounded-xl p-8 shadow-xl ${
-        dark
-          ? 'bg-gradient-to-b from-slate-700 via-slate-800  to-slate-900 text-white'
-          : ''
-      }`}
-    >
-      {children}
-    </motion.div>
-  );
-};
-
+const Card = ({ dark, children }: { dark?: boolean } & PropsWithChildren) => (
+  <div
+    className={`mb-8 w-full max-w-3xl items-center rounded-xl p-8 shadow-xl ${
+      dark
+        ? 'bg-gradient-to-b from-slate-700 via-slate-800  to-slate-900 text-white'
+        : ''
+    }`}
+  >
+    {children}
+  </div>
+);
 export default Card;
