b36841744c741325ed4d96079d3448300eb2d985
Tobias Ziegler <tobias.c.ziegler@gmail.com>
2023-03-14 21:32:28 +0100

    Add logo to header (#4)
    * Restructure layout
    
    * Update header
    
    * Add favicon
    
    * Update og image

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import logo from '../public/logo-main.webp';
 
 const Header = () => (
-  <nav className="mx-8 h-40">
-    <Link href="/" aria-label="Link zur Homepage"></Link>
-  </nav>
+  <header className="flex justify-center">
+    <nav className="w-full max-w-6xl p-8">
+      <Link href="/" aria-label="Link zur Homepage">
+        <Image
+          src={logo}
+          alt="Tobias Ziegler Logo"
+          className="sticky top-0"
+          width={50}
+          height={50}
+        />
+      </Link>
+    </nav>
+  </header>
 );
 
 export default Header;
